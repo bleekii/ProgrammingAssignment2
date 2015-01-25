bbd2f2b46fa2013c9de8a80e616f26eacbaf5328
bleekii <l337email@gmail.com>
2015-01-25 21:06:07 +0800

    Solved

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R
+## Functions: makeCacheMatrix
+##            cacheSolve
+##
 
-## Write a short comment describing this function
 
+## makeCacheMatrix
+##  input:  any square matrix
+##  output: a list including fuctions for acesses to the matrix and it's 
+##          inverse.
+##
+##  The initial version of the the list doesn't include the inverse. Use  
+##  cacheSolve() to solve the inverse.
+##
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## cacheSolve
+##  input:  a list created by makeCacheMatrix()
+##  output: returns the inverse of maxtrix supplied in the input. 
+##          
+##  The inverse is only calculated once otherwise a cached version is returned.
+##
+cacheSolve <- function(x, ...) {       
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
